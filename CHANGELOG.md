bb09fa7b671c7e4b9e4ad4727254abba4a44d4c1
TheRedBrain <thetrueredbrain@gmail.com>
2026-03-29 15:45:39 +0200

    setup

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# 1.0.0
+
+Adds features intended to give mobs that use the brain system additional functionality.
+#
--- src/client/java/com/github/theredbrain/mobbrainadditions/MobBrainAdditionsClient.java
+++ src/client/java/com/github/theredbrain/mobbrainadditions/MobBrainAdditionsClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.github.theredbrain.mobbrainadditions;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class MobBrainAdditionsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
